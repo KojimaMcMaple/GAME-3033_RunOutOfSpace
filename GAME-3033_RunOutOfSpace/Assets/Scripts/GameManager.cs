b152637f753a19f4fd01a6652238535b2b724467
KojimaMcMaple <KyleHunter.KH@outlook.com>
2022-02-27 23:39:44 -0500

    release #2

--- GAME-3033_RunOutOfSpace/Assets/Scripts/GameManager.cs
+++ GAME-3033_RunOutOfSpace/Assets/Scripts/GameManager.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private string next_level_;
+    [SerializeField] private string prev_level_;
+    [SerializeField] private GameObject overlay_panel_;
+    [SerializeField] private GameObject credits_panel_;
+    [SerializeField] private AudioClip click_sfx_;
+    private AudioSource audio_source_;
+    
     [SerializeField] private TMP_Text timer_txt_;
     [SerializeField] private float victory_timer_ = 300f;
     [SerializeField] private Animator door_anim_;
@@ -16,12 +25,17 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        audio_source_ = GetComponent<AudioSource>();
         timer_ = victory_timer_;
         player_ = GameObject.FindObjectOfType<Player.ThirdPersonController>();
     }
 
     private void FixedUpdate()
     {
+        if (player_ == null)
+        {
+            return;
+        }
         if (!is_times_up_ && !is_victory_)
         {
             if (timer_ > 0)
@@ -40,7 +54,83 @@ public class GameManager : MonoBehaviour
         if (player_.is_victory)
         {
             timer_txt_.text = "YOU WIN!";
+            DoShowOverlayPanel();
         }
+    }
+    
+    /// <summary>
+    /// Loads next level
+    /// </summary>
+    public void DoLoadNextLevel()
+    {
+        audio_source_.PlayOneShot(click_sfx_);
+        StartCoroutine(Delay());
+        SceneManager.LoadScene(next_level_);
+    }
+
+    /// <summary>
+    /// Loads prev level
+    /// </summary>
+    public void DoLoadPrevLevel()
+    {
+        audio_source_.PlayOneShot(click_sfx_);
+        StartCoroutine(Delay());
+        SceneManager.LoadScene(prev_level_);
+    }
+
+    /// <summary>
+    /// Closes app
+    /// </summary>
+    public void DoQuitApp()
+    {
+        audio_source_.PlayOneShot(click_sfx_);
+        StartCoroutine(Delay());
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Shows hidden panel
+    /// </summary>
+    public void DoShowOverlayPanel()
+    {
+        audio_source_.PlayOneShot(click_sfx_);
+        overlay_panel_.SetActive(true);
+    }
+
+    /// <summary>
+    /// Hides overlay panel
+    /// </summary>
+    public void DoHideOverlayPanel()
+    {
+        audio_source_.PlayOneShot(click_sfx_);
+        overlay_panel_.SetActive(false);
+    }
+
+    /// <summary>
+    /// Shows hidden panel
+    /// </summary>
+    public void DoShowCreditsPanel()
+    {
+        audio_source_.PlayOneShot(click_sfx_);
+        credits_panel_.SetActive(true);
+    }
+
+    /// <summary>
+    /// Hides overlay panel
+    /// </summary>
+    public void DoHideCreditsPanel()
+    {
+        audio_source_.PlayOneShot(click_sfx_);
+        credits_panel_.SetActive(false);
+    }
+
+    /// <summary>
+    /// General delay function for level loading, show explosion before game over, etc.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(2.0f);
     }
 
 }
